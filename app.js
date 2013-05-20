445dee66ae476ebbec417107038d2cb0e9b7060b
JackLee <freefoxser@gmail.com>
2013-05-20 22:00:32 +0800

    use node_modules express

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-
 /**
  * Module dependencies.
  */
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , http = require('http')
+    , path = require('path');
 
 var app = express();
 
@@ -23,12 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
